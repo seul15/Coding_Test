435132dc31ec2b066b292ec2330d21551bc64684
seul <angel3030303@gmail.com>
2024-11-21 20:35:30 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	long long dp[1001] = {};
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (dp[i] == 0) {
+			if (i == 1) dp[i] = 1%10007;
+			else if (i == 2) dp[i] = 1 + dp[i - 1]%10007;
+			else dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+		}
+		else continue;
+	}
+	cout << dp[n] << "\n";
+}
