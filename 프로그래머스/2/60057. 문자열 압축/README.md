56af8e1a3dcbf2a490c8e9a1e7f4bac73b74c434
seul <angel3030303@gmail.com>
2025-05-28 06:48:49 +0900

    [level 2] Title: 문자열 압축, Time: 2.27 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?gad_source=1&gclid=CjwKCAjw--K_BhB5EiwAuwYoyvumYgAjK-WDH3khaNwB07rFJYT6uszQiZhdRER0Wn4fdAHbOztTKBoCrL8QAvD_BwE) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 86.5 MB, 시간: 2.25 ms
+메모리: 76.9 MB, 시간: 2.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 23일 23:55:21
+2025년 05월 28일 06:48:46
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,37 +1,36 @@
 class Solution {
-    public int solution(String s) {
+    public int solution(String s){
         int answer = s.length();
-        
-        // 자르는 단위
+
         for(int i=1; i<=s.length()/2; i++){
-            int sub_num = 0;
+            int now_length = 0;
             String temp1 = s.substring(0,i);
-            int count = 1; // 반복되는 횟수
+            int count=1;
             for(int j=i; j<s.length(); j+=i){
-                // 현재 인덱스 + 자르는 단위가 문자열 길이를 넘지 않도록
                 String temp2;
-                if(j+i>s.length()){
-                    temp2 = s.substring(j);
+                if(j+i<=s.length()) temp2= s.substring(j,j+i);
+                else temp2=s.substring(j);
+
+                if(temp1.equals(temp2)) {
+                    count++;
+                    if(i+j>=s.length()){
+                        now_length +=i;
+                        if(count>1) now_length += String.valueOf(count).length();
+                    }
                 }
-                else temp2 = s.substring(j,j+i);
-                
-                if(temp2.equals(temp1)) count++;
                 else{
-                    if(count == 1) sub_num+=temp1.length();
-                    else sub_num+=(temp1.length() + String.valueOf(count).length());
+                    now_length +=i;
+                    if(count>1) now_length += String.valueOf(count).length();
                     temp1 = temp2;
-                    count = 1;
+                    count=1;
+
+                    if (j + i >= s.length()) {
+                        now_length += temp2.length();
+                    }
                 }
             }
-            if (count > 1) {
-                sub_num += String.valueOf(count).length() + temp1.length();
-            } else {
-                sub_num += temp1.length();
-            }
-            
-            answer = Math.min(answer, sub_num);
+            answer = Math.min(answer,now_length);
         }
-        
         return answer;
     }
 }
