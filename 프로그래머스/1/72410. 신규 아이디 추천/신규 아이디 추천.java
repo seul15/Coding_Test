d4529a46ef64d39b6b1600878aa699fccdd2e589
seul <angel3030303@gmail.com>
2025-05-27 14:24:02 +0900

    [level 1] Title: 신규 아이디 추천, Time: 14.21 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,27 +1,33 @@
+import java.util.*;
+
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
-        // 1단계
-        answer = new_id.toLowerCase();
-        // 2단계
+        String answer = new_id;
+        
+        answer = answer.toLowerCase();
+        
         answer = answer.replaceAll("[^a-z0-9-_.]","");
-        // 3단계
-        answer = answer.replaceAll("\\.{2,}", ".");
-        // 4단계
-        if(!answer.isEmpty() &&answer.charAt(0)=='.') answer = answer.substring(1);
-        if(!answer.isEmpty() &&answer.charAt(answer.length()-1)=='.') answer=answer.substring(0,answer.length()-1);
-        // 5단계
-        if(answer.isEmpty()) answer+="a";
-        // 6단계
-        if(answer.length()>=16){
-            answer=answer.substring(0,15);
-            if(answer.charAt(answer.length()-1)=='.') answer=answer.substring(0,answer.length()-1);
+        
+        while(answer.contains("..")){
+            answer = answer.replace("..",".");
         }
-        // 7단계
+        
+        if(!answer.isEmpty()&&answer.charAt(0) == '.') answer = answer.substring(1);
+        if(!answer.isEmpty()&&answer.charAt(answer.length()-1)=='.') answer = answer.substring(0,answer.length()-1);
+        
+        if(answer.isEmpty()) answer += "a";
+        
+        if(answer.length() >= 16){
+            answer = answer.substring(0,15);
+            if(answer.charAt(answer.length()-1) == '.'){
+                answer = answer.substring(0,answer.length()-1);
+            } 
+        }
+        
         if(answer.length()<=2){
             char temp = answer.charAt(answer.length()-1);
             while(answer.length()<3){
-                answer+=temp;
+                answer += temp;
             }
         }
         
