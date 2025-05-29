3abbd3c90c085b2c364adfb3c4df63ed96a23168
seul <angel3030303@gmail.com>
2025-05-30 00:44:15 +0900

    [level 3] Title: 양과 늑대, Time: 5.56 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,51 +1,38 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
     static List<Integer>[] graph;
     static int answer;
     
-    static void dfs(int node,int sheep, int wolf, List<Integer> next,int[] info){
-        visited[node] = true;
+    static void dfs(int node, int sheep, int wolf, List<Integer> next, int[] info){
         if(info[node] == 0) sheep++;
         else wolf++;
         
-        if(wolf>=sheep){
-            visited[node] = false;
-            return;
-        }
+        if(wolf>= sheep) return;
+        
         answer = Math.max(answer,sheep);
         
         List<Integer> copy = new ArrayList<>(next);
         copy.remove(Integer.valueOf(node));
+        copy.addAll(graph[node]);
         
-        for(int n: graph[node]){
-            copy.add(n);
-        } 
-
-        for(int n: copy){
-            if(!visited[n]){
-                dfs(n,sheep,wolf,copy,info);
-                visited[n] = false;
-            }
+        for(int i: copy){
+            dfs(i,sheep,wolf,copy,info);
         }
-        visited[node] = false;
     }
     
     public int solution(int[] info, int[][] edges) {
-        int len = info.length;
-        answer =0;
-        visited = new boolean[len];
-        graph = new ArrayList[len];
-        for(int i=0; i<len; i++){
+        graph = new ArrayList[info.length];
+        for(int i=0; i<info.length; i++){
             graph[i] = new ArrayList<>();
         }
-        for(int[] edge : edges){
+        for(int[] edge: edges){
             graph[edge[0]].add(edge[1]);
         }
+        
         List<Integer> next = new ArrayList<>();
         next.add(0);
-        dfs(0,0,0,next, info);
+        dfs(0,0,0,next,info);
         return answer;
     }
 }
