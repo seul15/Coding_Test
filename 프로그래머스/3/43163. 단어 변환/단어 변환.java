7974810432d2370e76bff8152e11f16e42f780c5
seul <angel3030303@gmail.com>
2025-05-30 11:36:37 +0900

    [level 3] Title: 단어 변환, Time: 0.49 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -4,36 +4,37 @@ class Solution {
     static Map<String,Integer> dist;
     
     static boolean check(String s1, String s2){
-        int count=0;
+        int count =0;
         for(int i=0; i<s1.length(); i++){
-            if(s1.charAt(i) != s2.charAt(i)) count++; 
+            if(s1.charAt(i) != s2.charAt(i)) count++;
         }
         return count==1;
     }
     
-    static int bfs(String begin, String target, String[] words){
+    static void bfs(String begin, String target, String[] words){
         Queue<String> q = new LinkedList<>();
         q.offer(begin);
         dist.put(begin,0);
         
         while(!q.isEmpty()){
-            String current = q.poll();
-            if(current.equals(target)) return dist.get(current); 
-            for(String w: words){
-                if(dist.get(w) == null && check(current,w)){
-                    q.offer(w);
-                    dist.put(w,dist.get(current)+1);
-                }
+            String cur = q.poll();
+            if(cur.equals(target)) return;
+            for(String next: words){
+               if(dist.get(next) == null && check(cur,next)){
+                   q.offer(next);
+                   dist.put(next,dist.get(cur)+1);
+               } 
             }
         }
-        return 0;
+        return;
     }
-    
     
     public int solution(String begin, String target, String[] words) {
         if(!Arrays.asList(words).contains(target)) return 0;
         dist = new HashMap<>();
         
-        return bfs(begin,target,words);
+        bfs(begin,target,words);
+        
+        return dist.getOrDefault(target,0);
     }
 }
