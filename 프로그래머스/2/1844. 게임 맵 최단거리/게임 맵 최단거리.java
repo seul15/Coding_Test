f61baca11c6db2ff6e3313ecd6e55181d1be25b9
seul <angel3030303@gmail.com>
2025-05-30 11:21:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.08 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     static boolean[][] visited;
-    static int[] dr = {1,-1,0,0};
-    static int[] dc = {0,0,1,-1};
-    static int N,M,answer;
+    static int[] dr ={1,-1,0,0};
+    static int[] dc= {0,0,1,-1};
+    static int N,M;
     
-    static boolean isValid(int r, int c){
+    static boolean isValid(int r,int c){
         return 0<=r && r<N && 0<=c && c<M;
     }
     
-    static int bfs(int r, int c,int[][] maps){
-        Queue<int[]> q = new LinkedList<>();
+    static int bfs(int r,int c, int[][] maps){
+        Queue<int[]> q= new LinkedList<>();
         q.offer(new int[]{r,c,1});
         visited[r][c] = true;
         while(!q.isEmpty()){
-            int[] current = q.poll();
-            int cr = current[0]; int cc = current[1]; int count = current[2];
+            int[] cur =q.poll();
+            int cr = cur[0]; int cc = cur[1]; int count = cur[2];
             if(cr==N-1 && cc==M-1) return count;
             for(int i=0; i<4; i++){
-                int nr= cr+dr[i]; int nc=cc+dc[i];
-                if(isValid(nr,nc) && maps[nr][nc] == 1 && !visited[nr][nc]){
-                    q.offer(new int[]{nr,nc,count+1});
+                int nr = cr+dr[i]; int nc = cc+dc[i];
+                if(isValid(nr,nc) && maps[nr][nc]==1 && !visited[nr][nc]){
+                    q.offer(new int[] {nr,nc,count+1});
                     visited[nr][nc] = true;
                 }
             }
@@ -30,9 +30,7 @@ class Solution {
     }
     
     public int solution(int[][] maps) {
-        answer = Integer.MAX_VALUE;
-        N = maps.length;
-        M = maps[0].length;
+        N = maps.length; M = maps[0].length;
         visited = new boolean[N][M];
         
         return bfs(0,0,maps);
