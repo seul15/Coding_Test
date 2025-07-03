ca45bf8e140d90a1b1e130e9fe728f07d7902f36
seul <angel3030303@gmail.com>
2025-07-03 17:36:46 +0900

    [level 2] Title: 소수 찾기, Time: 36.67 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 96.9 MB, 시간: 21.67 ms
+메모리: 84 MB, 시간: 36.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 09:47:39
+2025년 07월 03일 17:36:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,30 +1,33 @@
 import java.util.*;
+
 class Solution {
-    static Set<Integer> s = new HashSet<>();
     static boolean[] visited;
+    static Set<Integer> n = new HashSet<>();
+    
     static boolean check(int num){
         if(num<2) return false;
         for(int i=2; i<=Math.sqrt(num); i++){
-            if(num % i == 0) return false;  
+            if(num %i ==0) return false; 
         }
         return true;
     }
     
     static void backtracking(String temp, String numbers){
         if(!temp.isEmpty() && check(Integer.parseInt(temp))){
-            s.add(Integer.parseInt(temp));
+            n.add(Integer.parseInt(temp));
         }
-           for(int i=0; i<numbers.length(); i++){
-               if(visited[i]) continue;
-               visited[i] = true;
-               backtracking(temp+numbers.charAt(i),numbers);
-               visited[i] = false;
-           }
+        for(int i=0; i<numbers.length(); i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            backtracking(temp+numbers.charAt(i),numbers);
+            visited[i] = false;
+        }
     }
+    
     
     public int solution(String numbers) {
         visited = new boolean[numbers.length()];
         backtracking("",numbers);
-        return s.size();
+        return n.size();
     }
 }
