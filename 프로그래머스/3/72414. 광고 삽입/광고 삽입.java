6380010f221f39244b171b66d17ee3c4f7c22227
seul <angel3030303@gmail.com>
2025-07-03 16:48:16 +0900

    [level 3] Title: 광고 삽입, Time: 347.50 ms, Memory: 287 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,19 +1,20 @@
+import java.util.*;
+
 class Solution {
     static int second(String time){
-        String[] temp = time.split(":");
-        return Integer.parseInt(temp[0]) * 3600 +
-            Integer.parseInt(temp[1]) * 60+
-            Integer.parseInt(temp[2]);
+        String[] t = time.split(":");
+        return Integer.parseInt(t[0]) * 3600 +
+            Integer.parseInt(t[1])*60 +
+            Integer.parseInt(t[2]);
     }
     
-    static String time(int num){
-        int h = num/3600;
-        num %= 3600;
-        int m = num /60;
-        int s = num %60;
+    static String time(int sec){
+        int h = sec/3600;
+        sec %= 3600;
+        int m = sec/60;
+        int s = sec%60;
         return String.format("%02d:%02d:%02d",h,m,s);
     }
-    
     
     public String solution(String play_time, String adv_time, String[] logs) {
         int play = second(play_time);
@@ -21,9 +22,9 @@ class Solution {
         long[] prefix = new long[play+1];
         
         for(String log: logs){
-            String[] t = log.split("-");
-            int start = second(t[0]);
-            int end = second(t[1]);
+            String[] l = log.split("-");
+            int start = second(l[0]);
+            int end = second(l[1]);
             prefix[start]++;
             prefix[end]--;
         }
@@ -31,16 +32,15 @@ class Solution {
         for(int i=1; i<=play; i++) prefix[i] += prefix[i-1];
         for(int i=1; i<=play; i++) prefix[i] += prefix[i-1];
         
-        long maxview = prefix[adv-1];
-        int answer = 0;
-        for(int i=adv; i<=play; i++){
-            long cur = prefix[i] - prefix[i-adv];
-            if(cur>maxview){
-                maxview = cur;
-                answer = i-adv+1;
+        long maxview = prefix[adv];
+        int maxtime =0;
+        for(int i= adv; i<=play; i++){
+            long curview = prefix[i] - prefix[i-adv];
+            if(curview> maxview){
+                maxview = curview;
+                maxtime = i-adv+1;
             }
         }
-        
-        return time(answer);
+        return time(maxtime);
     }
 }
