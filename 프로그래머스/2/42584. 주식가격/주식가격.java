f44ef37bf5706b3d8f4cbd859bbbb1a8493cb025
seul <angel3030303@gmail.com>
2025-11-27 14:12:48 +0900

    [level 2] Title: 주식가격, Time: 36.15 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,15 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
+        Stack<Integer> s = new Stack<>();
         int[] answer = new int[prices.length];
-        for(int i=0; i<prices.length-1; i++){
-            int count=0;
-            for(int j=i+1; j<prices.length; j++){
-                count++;
-                if(prices[i]>prices[j]) break;
+        for(int i=0; i<prices.length; i++){
+            while(!s.isEmpty() && prices[s.peek()]>prices[i]){
+                int temp = s.pop();
+                answer[temp] = i-temp;
             }
-            answer[i] = count;
+            s.push(i);
         }
-        answer[prices.length-1]=0;
+        while(!s.isEmpty()){
+            int temp = s.pop();
+            answer[temp] = prices.length-1 - temp;  
+        }
         return answer;
     }
 }
