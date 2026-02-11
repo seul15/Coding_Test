c4ccb27593fef05c6a78e97e87bb00e4a896dd93
seul <angel3030303@gmail.com>
2026-02-11 20:32:13 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.58 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
 
 ### 성능 요약
 
-메모리: 89.9 MB, 시간: 7.84 ms
+메모리: 88 MB, 시간: 6.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 30일 10:13:02
+2026년 02월 11일 20:32:09
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,50 +1,53 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
-    static List<Integer>[] graph;
-    
-    static int bfs(int n){
-        int count=1;
-        Arrays.fill(visited, false);
+    int bfs(ArrayList<Integer>[] graph,boolean[] visited, int n){
         Queue<Integer> q = new LinkedList<>();
-        q.offer(n);
-        visited[n] = true; 
+        q.offer(1);
+        visited[1] =true;
+        int cnt =1;
         while(!q.isEmpty()){
-            int current = q.poll();
-            for(int w: graph[current]){
-                if(!visited[w]){
-                    count++;
-                    visited[w] = true;
-                    q.offer(w);
-                }
+            int now = q.poll();
+            for(int num: graph[now]){
+                if(visited[num]) continue;
+                q.offer(num);
+                cnt++;
+                visited[num] = true;
             }
         }
-        return count;
+        return Math.abs(cnt-(n-cnt));
     }
     
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        visited = new boolean[n];
-        graph = new ArrayList[n];
-        for(int i=0; i<n ; i++){
+        int answer =Integer.MAX_VALUE;
+        ArrayList<Integer>[] graph = new ArrayList[n+1];
+        
+        for(int i=1; i<=n; i++){
             graph[i] = new ArrayList<>();
         }
-        for(int[] wire : wires){
-            graph[wire[0]-1].add(wire[1]-1);
-            graph[wire[1]-1].add(wire[0]-1);
+        
+        for(int[] w: wires){
+            int a = w[0];
+            int b = w[1];
+            graph[a].add(b);
+            graph[b].add(a);
         }
         
-        for(int[] wire : wires){
-            graph[wire[0]-1].remove(Integer.valueOf(wire[1]-1));
-            graph[wire[1]-1].remove(Integer.valueOf(wire[0]-1));
+        for(int[] w: wires){
+            // 간선 제거
+            int a = w[0];
+            int b = w[1];
             
-            int count = bfs(wire[0]);
-            answer = Math.min(answer, Math.abs(n-2*count));
+            graph[a].remove(Integer.valueOf(b));
+            graph[b].remove(Integer.valueOf(a));
             
-            graph[wire[0]-1].add(wire[1]-1);
-            graph[wire[1]-1].add(wire[0]-1);
+            answer = Math.min(answer, bfs(graph, new boolean[n+1], n));
+            
+            // 다시 간선 추가
+            graph[a].add(b);
+            graph[b].add(a);
         }
+        
         return answer;
     }
 }
