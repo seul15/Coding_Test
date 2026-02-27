1b46e29de0bb564ba2c45c954f1045f79c6699bd
seul <angel3030303@gmail.com>
2026-02-27 21:13:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.82 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,48 @@
 import java.util.*;
 
-class Solution {
-    static boolean[][] visited;
-    static int[] dr ={1,-1,0,0};
-    static int[] dc= {0,0,1,-1};
-    static int N,M;
-    
-    static boolean isValid(int r,int c){
-        return 0<=r && r<N && 0<=c && c<M;
-    }
-    
-    static int bfs(int r,int c, int[][] maps){
-        Queue<int[]> q= new LinkedList<>();
-        q.offer(new int[]{r,c,1});
-        visited[r][c] = true;
+class Solution {  
+    int bfs(int[][] maps){
+        // 상하좌우 이동을 위한 배열
+        int[] dx= {0,0,1,-1};
+        int[] dy= {-1,1,0,0};
+        int N = maps[0].length; // 가로
+        int M = maps.length; // 세로
+        boolean[][] visited = new boolean[M][N];
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0,0,1});
+        visited[0][0] = true;
         while(!q.isEmpty()){
-            int[] cur =q.poll();
-            int cr = cur[0]; int cc = cur[1]; int count = cur[2];
-            if(cr==N-1 && cc==M-1) return count;
+            int[] now = q.poll();
+            int nx = now[0];
+            int ny = now[1];
+            int nmove = now[2];
+            
             for(int i=0; i<4; i++){
-                int nr = cr+dr[i]; int nc = cc+dc[i];
-                if(isValid(nr,nc) && maps[nr][nc]==1 && !visited[nr][nc]){
-                    q.offer(new int[] {nr,nc,count+1});
-                    visited[nr][nc] = true;
-                }
+                int nextx = nx+dx[i];
+                int nexty = ny+dy[i];
+
+                // 맵을 벗어났는지 판단
+                if(0>nextx || nextx>=N ||
+                  0>nexty || nexty >=M) continue;
+                
+                // 맵을 지나갈 수 있는지 판단
+                if(maps[nexty][nextx] == 0) continue;
+                
+                // 방문 여부 판단
+                if(visited[nexty][nextx]) continue;
+                
+                // 도착 지점인지 판단
+                if(nextx == N-1 && nexty == M-1) return nmove+1;
+                
+                q.offer(new int[]{nextx, nexty, nmove+1});
+                visited[nexty][nextx] = true;
             }
         }
         return -1;
     }
     
     public int solution(int[][] maps) {
-        N = maps.length; M = maps[0].length;
-        visited = new boolean[N][M];
-        
-        return bfs(0,0,maps);
+        return bfs(maps);
     }
 }
