22b3f7981882bcb93c301f2d7b56d08fa9ffdc5d
seul <angel3030303@gmail.com>
2026-02-28 11:13:53 +0900

    [level 3] Title: 단어 변환, Time: 0.63 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,40 +1,53 @@
 import java.util.*;
 
 class Solution {
-    static Map<String,Integer> dist;
-    
-    static boolean check(String s1, String s2){
-        int count =0;
-        for(int i=0; i<s1.length(); i++){
-            if(s1.charAt(i) != s2.charAt(i)) count++;
+    class State{
+        String word;
+        int depth;
+        
+        public State(String word, int depth){
+            this.word = word;
+            this.depth = depth;
         }
-        return count==1;
     }
     
-    static void bfs(String begin, String target, String[] words){
-        Queue<String> q = new LinkedList<>();
-        q.offer(begin);
-        dist.put(begin,0);
+    boolean check(String A, String B){
+        int count =0;
+        for(int i=0; i<A.length(); i++){
+            if(A.charAt(i) != B.charAt(i)) count++;
+            if(count>1) return false;
+        }
         
+        return (count==1)?true: false;
+    }
+    
+    int bfs(String begin, String target, String[] words){
+        boolean[] visited = new boolean[words.length];
+        Queue<State> q = new LinkedList<>();
+        
+        q.offer(new State(begin, 0));
         while(!q.isEmpty()){
-            String cur = q.poll();
-            if(cur.equals(target)) return;
-            for(String next: words){
-               if(dist.get(next) == null && check(cur,next)){
-                   q.offer(next);
-                   dist.put(next,dist.get(cur)+1);
-               } 
+            State now = q.poll();
+            String nw = now.word;
+            int nd = now.depth;
+            
+            for(int i=0; i<words.length; i++){
+                if(check(nw, words[i]) && !visited[i]){
+                    if(words[i].equals(target)) return nd+1;
+                    q.offer(new State(words[i],nd+1));
+                    visited[i] = true;
+                }
             }
         }
-        return;
+        return 0;
     }
     
     public int solution(String begin, String target, String[] words) {
-        if(!Arrays.asList(words).contains(target)) return 0;
-        dist = new HashMap<>();
-        
-        bfs(begin,target,words);
-        
-        return dist.getOrDefault(target,0);
+        for(int i=0; i<words.length; i++){
+            if(words[i].equals(target)){
+                return bfs(begin, target, words);   
+            }
+        }
+        return 0;
     }
 }
