0622b441b54ad6db7a47920a0d3b7afd42a7d705
seul <angel3030303@gmail.com>
2026-06-20 18:25:19 +0900

    [level 2] Title: 타겟 넘버, Time: 2.91 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 98 MB, 시간: 4.38 ms
+메모리: 75.2 MB, 시간: 2.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 27일 20:13:18
+2026년 06월 20일 18:25:16
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,19 @@
 class Solution {
-    int answer;
+    int answer=0;
     
-    void dfs(int now, int index, int[] numbers, int target){
+    void dfs(int sum, int index, int[] numbers, int target){
         if(index == numbers.length){
-            if(now == target) answer++;
+            if(target == sum) answer++;
             return;
         }
         
-        // 다음 요소를 +하는 
-        dfs(now+numbers[index], index+1, numbers,target);
-        // 다음 요소를 -하는 경우
-        dfs(now-numbers[index], index+1, numbers, target);
+        dfs(sum+numbers[index], index+1 , numbers, target);
+        dfs(sum-numbers[index],index+1, numbers ,target);
     }
     
     public int solution(int[] numbers, int target) {
         answer = 0;
-        dfs(0,0,numbers, target);
-        
+        dfs(0,0,numbers,target);
         return answer;
     }
 }
