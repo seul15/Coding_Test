50c5db456697f7b735dc052cd2fa5f62f7fb27d6
seul <angel3030303@gmail.com>
2026-06-20 19:14:29 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,32 @@
 import java.util.*;
 
 class Solution {
-    int bfs(int n, int[][] computers){
-        int answer =0;
-        boolean[] visited = new boolean[n];
+    int answer =0;
+
+    void bfs(int com, boolean[] visited, int[][] computers, int n){
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i<n; i++){
-            if(visited[i]) continue;
-            
-            // 새로운 네트워크 발견
-            q.offer(i);
-            visited[i] = true;
-            answer++; 
-            while(!q.isEmpty()){
-                int now = q.poll();
-                for(int next=0; next<n; next++){
-                    if(computers[now][next] == 1){
-                        if(visited[next]) continue;
-                    
-                        q.offer(next);
-                        visited[next] = true;   
-                    }
+        q.offer(com);
+        answer++;
+        
+        while(!q.isEmpty()){
+            int now = q.poll();
+            visited[now] = true;
+            for(int i=0; i<n; i++){
+                if(!visited[i] && now!=i && computers[now][i]==1){
+                    q.offer(i);
+                    visited[i] = true;
                 }
             }
         }
-        return answer;
     }
     
     public int solution(int n, int[][] computers) {
+        answer = 0;
+        boolean[] visited = new boolean[n];
         
-        return bfs(n,computers);
+        for(int i=0; i<n; i++){
+            if(!visited[i]) bfs(i,visited,computers, n);
+        }
+        return answer;
     }
 }
